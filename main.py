893aa41584b2359504778452b27b311f9c588c5b
Deepika Sharma <deepikaasharma@outlook.com>
2020-08-26 22:51:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Create a list
+some_list = ['man', 'bear', 'pig']
+
+# Unpack the list
+man, bear, pig = some_list
+
+'''
+The statement above is equivalent to:
+
+man = some_list[0]
+bear = some_list[1]
+pig = some_list[2]
+'''
+
+# Show that the variables represent the values of the original list
+print(man, bear, pig)
+print(some_list)
